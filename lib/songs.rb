45842a635fd61ecfc44fcef722de40ca1df3a3d6
cowboy-mike <mike@mnanto.com>
2021-03-13 14:46:35 -0800

    create scraper class + CSS elements

---
+++ lib/songs.rb
@@ -0,0 +1,14 @@
+class Song
+    attr_accessor :title, :artist, :last_week, :peak, :weeks_on_chart
+
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
